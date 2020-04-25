0e77031ed10f62cec28f147548392189adafcb08
EduardoAraujoB <ea0123450@gmail.com>
2020-04-25 15:24:19 -0300

    creating a basic typescript project structure

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+import "./bootstrap";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json("Hello World");
+});
+
+app.use(express.json());
+
+app.listen(process.env.APP_PORT);
+console.log(`🚀  Server ready at ${process.env.APP_URL}`);
