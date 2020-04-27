7150aab4dad9f9e6c121eae0ba00fb5127b73f9c
Lucas Montano <lucasmontano@gmail.com>
2020-04-27 14:34:40 +0200

    fetching chat participants and incrementing points

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,38 @@
 import express from 'express';
 
+import axios from 'axios';
+
 import './bootstrap';
 
 const app = express();
 
+let viewerwersPoint = new Map();
+
+setInterval(function () {
+  axios.get('https://tmi.twitch.tv/group/user/lucas_montano/chatters')
+    .then(function (response) {
+      // handle success
+      response.data.chatters.viewers.forEach(viewer => {
+        let point = 0
+        if (viewerwersPoint.has(viewer)) {
+          point = viewerwersPoint.get(viewer)
+        }
+        point++
+        viewerwersPoint.set(viewer, point)
+        console.log(`${viewer} have ${point} points`);
+      });
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+}, 60 * 1000);
+
 app.get('/', (req, res) => {
-  res.json('Hello World');
+  res.json('Hello Twitch! :)');
 });
 
 app.use(express.json());
