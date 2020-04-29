7db11e733beb54ee4ca04798b9aa04f64b45a2e8
Lucas Montano <lucasmontano@gmail.com>
2020-04-29 18:54:58 +0200

    adding mongodb and storing participants points

--- src/app.ts
+++ src/app.ts
@@ -5,30 +5,66 @@ import ChatterService from './services/ChatterService';
 const app = express();
 app.use(express.json());
 
-const participants = new Map();
+let topParticipants = new Array();
 
-setInterval(async () => {
-  try {
-    const viewers = await ChatterService.getViewers();
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  if (err) throw err;
 
-    viewers.forEach((viewer) => {
-      let point = participants.get(viewer) ?? 0;
-      participants.set(viewer, ++point);
-      console.log(`${viewer} have ${point} points`);
+  const db = client.db(process.env.DATABASE_NAME);
+  const participantsCollection = db.collection('participants');
+  fetchTopParticipants(participantsCollection);
+  setInterval(async () => {
+    try {
+      const viewers = await ChatterService.getViewers();
+
+      viewers.forEach(async (viewer) => {
+        await incrementParticipantPoints(participantsCollection, viewer);
+      });
+
+      fetchTopParticipants(participantsCollection);
+    } catch (error) {
+      console.log(error);
+    }
+  }, 60 * 1000);
+});
+
+function fetchTopParticipants(participantsCollection: any) {
+  participantsCollection
+    .find()
+    .sort({ points: -1 })
+    .limit(10)
+    .toArray((err, document) => {
+      if (err) throw err;
+      topParticipants = document.map((participant) => {
+        return { name: participant.name, points: participant.points };
+      });
     });
-  } catch (error) {
-    console.log(error);
-  }
-}, 60 * 1000);
+}
+
+async function incrementParticipantPoints(
+  participantsCollection: any,
+  viewer: string
+) {
+  await participantsCollection
+    .updateOne({ name: viewer }, { $inc: { points: 1 } })
+    .then((result) => {
+      if (!result.modifiedCount) {
+        participantsCollection.insertOne({
+          name: viewer,
+          points: 1,
+        });
+      }
+    })
+    .catch((err) => console.error(`Failed to add review: ${err}`));
+}
 
 app.get('/', (req, res) => {
-  const factoryViewer = ([name, points]) => ({ name, points });
-  const viewers = Array.from(participants).map(factoryViewer);
-
-  const top10 = viewers
-    .sort((a, b) => (a.points > b.points ? 1 : -1))
-    .slice(0, 10);
-  return res.json(top10);
+  return res.json(topParticipants);
 });
 
 export default app;
