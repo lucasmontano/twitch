c6d15f94c1263666612679a2aa241d0cc6b1a0f0
Lucas Montano <lucasmontano@gmail.com>
2020-04-29 20:19:47 +0200

    ignoring lintter on ChatterService

--- src/services/ChatterService.ts
+++ src/services/ChatterService.ts
@@ -17,12 +17,13 @@ interface IResponse {
 }
 
 class ChatterService {
+  // eslint-disable-next-line class-methods-use-this
   public async getViewers(): Promise<Participants> {
-    this.response = await axios.get<IResponse>(
+    const response = await axios.get<IResponse>(
       'https://tmi.twitch.tv/group/user/lucas_montano/chatters',
     );
 
-    const { viewers } = this.response.data.chatters;
+    const { viewers } = response.data.chatters;
 
     return viewers;
   }
