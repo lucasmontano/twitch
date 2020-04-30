5b0811f39585f4a8a4e128e16188209e73569ea1
Murilo Henrique <oofleaoo@gmail.com>
2020-04-30 16:09:20 -0300

    Add feature to remove bots from viewers list

--- src/services/ChatterService.ts
+++ src/services/ChatterService.ts
@@ -6,14 +6,24 @@ interface IResponse {
   _links: {
     [key: string]: string;
   };
+  chatter_count: number;
   chatters: {
     broadcaster: string[];
     vips: string[];
     moderators: string[];
     staff: string[];
     admins: string[];
+    global_mods: string[];
     viewers: string[];
   };
+}
+
+interface Bot {
+  [key:string]: string;
+}
+
+interface BotData {
+  bots: Bot[];
 }
 
 class ChatterService {
@@ -25,7 +35,14 @@ class ChatterService {
 
     const { viewers } = response.data.chatters;
 
-    return viewers;
+    return this.removeBots(viewers);
+  }
+
+  private async removeBots(viewers: Participants): Promise<Participants> {
+    const response = await axios.get<BotData>('https://api.twitchinsights.net/v1/bots/online');
+    const botNames = new Set(response.data.bots.map((bot) => bot[0]));
+    const filteredViewers = viewers.filter((viewer) => !botNames.has(viewer));
+    return filteredViewers;
   }
 }
 
