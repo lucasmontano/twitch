3ce499f9bcf953f2f3ebdb412a7adef31972434c
nataneb32 <nataestanislaubastos@gmail.com>
2020-05-02 02:55:05 -0300

    moving function from utils to ParticipantRepo

--- src/repository/ParticipantRepository.ts
+++ src/repository/ParticipantRepository.ts
@@ -16,6 +16,22 @@ class ParticipantRepository {
   public async insertOne(docs: Pick<Participant, "name" | "points"> & {_id?: ObjectId; }){
     return this.participantCollection.insertOne(docs)
   }
+
+  public async fetchTopParticipants(): Promise<Participant[]> {
+    return this.participantCollection
+      .find({}, { projection: { name: 1, points: 1, _id: 0 } })
+      .sort({ points: -1 })
+      .limit(10)
+      .toArray();
+  }
+
+  public async incrementParticipantPoints(name: string): Promise<any> {  
+    return this.participantCollection.updateOne({ name }, {
+      $inc: {
+        points: 1,
+      },
+    });
+  }
 } 
 
 export default new ParticipantRepository()
